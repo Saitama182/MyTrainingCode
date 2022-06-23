cd27102ab91552ce256394b0378ee91261ad82e3
Evgeny <onepiece182tom@gmail.com>
2022-06-23 19:10:47 +0300

    закончил на главе 29

---
+++ Task29/Task29.1.js
@@ -0,0 +1,3 @@
+// Дана строка '12345'. Найдите сумму цифр этой строки.
+let str = '12345';
+console.log(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4]));
---
+++ Task29/Task29.2.js
@@ -0,0 +1,4 @@
+// Дано число 12345. Найдите сумму цифр этого числа.
+let num = 12345;
+let str = String(num);
+console.log(Number(str[0]) + Number(str[1]) + Number(str[2]) + Number(str[3]) + Number(str[4]));
